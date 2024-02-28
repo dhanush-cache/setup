386d8317e031658303172fc6a5c33a573820a301
Anonymous <anonymous@example.com>
2024-02-29 00:04:08 +0530

    Add a method to create folders in the project's root directory.

--- setup.py
+++ setup.py
@@ -9,3 +9,9 @@ class Setup:
     def __init__(self):
         self.pwd = Path().absolute()
         self.on_unix = False if platform.system() == "Windows" else True
+
+    def mkdir(self, *folders):
+        """Create folders in the root directory."""
+        for folder in folders:
+            if not Path(folder).exists():
+                Path(folder).mkdir()
